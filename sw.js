3d688ca56ab94f8eaaf333021a6a5e43f335f9df
t777r6jpsw-png <t777r6jpsw@privaterelay.appleid.com>
2026-09-13 18:37:00 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,5 @@
-const CACHE='tokyo-trip-2026-v3';
+const CACHE='tokyo-trip-2026-v1-fixed';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{
- const req=e.request;
- if(req.mode==='navigate' || new URL(req.url).pathname.endsWith('/index.html')){
-  e.respondWith(fetch(req).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(req,copy));return r}).catch(()=>caches.match(req)));
- }else e.respondWith(caches.match(req).then(r=>r||fetch(req)));
-});
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
