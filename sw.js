a0c3d2f641db693993a200f8fcc04ad63262f6d3
t777r6jpsw-png <t777r6jpsw@privaterelay.appleid.com>
2026-09-13 17:46:48 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE='tokyo-trip-2026-v6';
+const CACHE='tokyo-trip-2026-v7';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
- const req=e.request;
- if(req.mode==='navigate' || new URL(req.url).pathname.endsWith('/index.html')){
-  e.respondWith(fetch(req).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(req,copy));return r}).catch(()=>caches.match(req)));
- }else e.respondWith(caches.match(req).then(r=>r||fetch(req)));
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put('./index.html',c));return r;}).catch(()=>caches.match('./index.html')));
+  }else{
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  }
 });
